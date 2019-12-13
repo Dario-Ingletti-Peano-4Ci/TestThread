958d53a20a80faca096de4a05fe7203480ad1f1f
Dario-Ingletti-Peano-4C <56158433+SSOB-dax@users.noreply.github.com>
2019-12-13 13:45:46 +0100

    Add files via upload
    Ho creato tutte le classi richieste e ho constatato che più thread possono essere eseguiti contemporaneamente

---
+++ Say.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsaluto;
+
+/**
+ *
+ * @author martone.christian
+ */
+public class Say extends Thread {
+
+    public void run(String cosaDire) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(cosaDire);
+        }
+    }
+}
